1983e38ddf4c475f5c3e9ac819280dc5d2a14ce1
Brandon <brandon.monier@gmail.com>
2024-12-03 14:18:00 -0600

    Add Day 3 solutions

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,9 +1,37 @@
 fun main() {
+    fun mulExtract(s: String): List<Pair<Int, Int>> {
+        val regex = """mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
+
+        val match = regex.findAll(s)
+            .map { match ->
+                val (n1, n2) = match.destructured
+                n1.toInt() to n2.toInt()
+            }
+            .toList()
+
+        return match
+    }
+
+    fun findCompatMul(s: String): List<String> {
+        val regex = """(?:^|do\(\))(.*?)(?:don't\(\)|$)""".toRegex(RegexOption.DOT_MATCHES_ALL)
+
+        val match = regex.findAll(s)
+            .map { match ->
+                match.groupValues[0]
+            }
+            .toList()
+
+        return match
+    }
+
+    val dayInput = readInput("day_03").joinToString("")
+
     // Part 1
-    val res01 = null
+    val res01 = mulExtract(dayInput).sumOf { it.first * it.second }
 
     // Part 2
-    val res02 = null
+    val res02 = findCompatMul(dayInput).sumOf { mulExtract(it).sumOf { it.first * it.second } }
+
 
     println("Part 1 answer: $res01")
     println("Part 2 answer: $res02")
